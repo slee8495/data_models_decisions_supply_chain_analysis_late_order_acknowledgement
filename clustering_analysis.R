d7183b5422819dcbd8fd7075ab93db10228d4741
rsm-sal072 <sal072@ucsd.edu>
2024-02-26 19:41:02 -0800

    02262024

---
+++ clustering_analysis.R
@@ -0,0 +1,63 @@
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(DT)
+library(rmarkdown)
+library(shinyjs)
+
+ui <- fluidPage(
+  
+  id = "main_nav",
+  title = (""),
+  tabPanel("",
+           fluidPage(
+             pickerInput("chapter", "",
+                         choices = c("Case Summary", 
+                                     "Analysis")),
+             uiOutput("chapter")))
+  
+  
+  
+  
+)
+
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+##########################################################################################################################################
+
+
+
+server <- function(input, output, session) {
+  active_tab <- reactiveVal("chapter")
+  
+  
+  ################################## Chapter 1: Descision Analysis ###########################################
+  
+  
+  output$chapter <- renderUI({
+    req(input$chapter) 
+    
+    file_name <- switch(input$chapter,
+                        "Case Summary" = "summary_clustering.html",
+                        "Analysis" = "under_construction.html")
+    
+    if (!is.null(file_name)) {
+      tags$iframe(src = file_name, style = "width:100%; height:800px;")
+    }
+  })
+  
+  observeEvent(input$main_nav, {
+    active_tab(input$main_nav)
+  })
+  
+  
+  #########################################################################################################################################################
+  
+  
+}
+
+shinyApp(ui = ui, server = server)
+
+
